223aea87ee51e2ecccca517f5ca5038e32b830f4
anjomro <github@anjomro.de>
2024-06-16 22:01:32 +0200

    Implement new ICE logon portal

--- src/escape_captivity/solver/ice.py
+++ src/escape_captivity/solver/ice.py
@@ -17,9 +17,12 @@ class IceSolver(BaseSolver):
     def solve(self, portal_url: ParseResult) -> bool:
         # Generate some random CSRF token
         csrf_token = ''.join(random.choices(string.ascii_letters + string.digits, k=33))
-        r = httpx.post(f"https://{portal_url.netloc}/de/",
-                          data={"login": "true", "CSRFToken": csrf_token}, cookies=dict(csrf=csrf_token),
-                          follow_redirects=False)
+        if "cna" in portal_url.path:
+            r = httpx.post(f"https://{portal_url.netloc}/cna/logon", follow_redirects=False)
+        else:
+            r = httpx.post(f"https://{portal_url.netloc}/de/",
+                              data={"login": "true", "CSRFToken": csrf_token}, cookies=dict(csrf=csrf_token),
+                              follow_redirects=False)
 
         if r.status_code in [302, 301, 200]:
             return True
